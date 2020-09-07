939ca6b320d219dbe94e9c3412dabdc40eaa53b9
Romain Balestreri <romain.balestreri@orange.fr>
2020-09-07 15:36:32 +0200

    first try

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import GameList from './GameList';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {sortType:null}
+  }
+  sortByAlphaAsc() {
+    this.setState({sortType:"alpha-asc"})
+  }
+  sortByAlphaDesc() {
+    this.setState({sortType:"alpha-desc"})
+  }
+  sortByDateAsc() {
+    this.setState({sortType:"date-asc"})
+  }
+  sortByDateDesc() {
+    this.setState({sortType:"date-desc"})
+  }
+  resetSort() {
+    this.setState({sortType:null})
+  }
+  componentDidMount() {
+    //init
+  }
+  componentWillUnmount() {
+    //fin de vie
+  }
   render() {
     return (
+      <React.Fragment>
+      <header className="sortbar">
+        <button id="sort-alpha-asc" className="btn" onClick={this.sortByAlphaAsc().bind(this)}>Trier par ordre alphabétique</button>
+        <button id="sort-alpha-desc" className="btn" onClick={this.sortByAlphaDesc().bind(this)}>Trier par ordre alphabétique inversé</button>
+        <button id="sort-date-desc" className="btn" onClick={this.sortByDateDesc().bind(this)}>Trier par date de sortie la plus récente</button>
+        <button id="sort-date-asc" className="btn" onClick={this.sortByDateAsc().bind(this)}>Trier par date de sortie la moins récente</button>
+        <button id="reset" className="btn" onClick={this.resetSort().bind(this)}>Réinitialiser</button>
+      </header>
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <GameList sortType={this.state.sortType} />
       </div>
+      </React.Fragment>
     );
   }
 }
---
+++ src/GameList.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import GameList from './GameList';
+
+test('renders learn react link', () => {
+  /*const { getByText } = render(<GameList />);
+  const linkElement = getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();*/
+});
